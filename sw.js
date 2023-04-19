e02a8543c99796ddc3269cca27cade6cde7db667
Emma Nielsen <Emmanielsen843@gmail.com>
2023-04-19 09:14:30 +0200

    comments

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
+// Navn på App Shell Cache til assets filer
 const staticCacheName = 'site-static-v1'
+// Navn på dynamisk cache
 const dynamicCacheName = 'site-dynamic-v1'
 
+// Array med assets filer til statisk cache
 const assets = [
     '/',
     './index.html',
@@ -17,9 +20,14 @@ const assets = [
 // Her installere vi vores service worker
 self.addEventListener('install', event => {
     // console.log('serviceworker has been installed');
+    // Vent til alle opgaver er udført
     event.waitUntil(
+
+        /* Tilføj assets filer til statisk cache*/
+
+        // Åbn statisk cache
         caches.open(staticCacheName).then(cache => {
-            // console.log(('Caching all asstes'));
+            // Tilføj array af assets filer til cache
             cache.addAll(assets)
         })
     )
@@ -28,20 +36,28 @@ self.addEventListener('install', event => {
 // Efter sw er blevet installeret skal den aktiveres. Vi aktivere den ved hjælp af 'active'
 self.addEventListener('activate', event => {
     console.log('serviceworker has been activated');
-
+    // Vent til alle opgaver er udført
     event.waitUntil(
+
+        /* Slet tidligere cache versioner */
+
+        // Kald alle cache nøgler (navn på cache samlinger)
         caches.keys().then(keys => {
+            // Returnerer et array af promise (et promis for hver fil)
             return Promise.all(keys
+                // Filtrer alle som ikke er medlem af den nuværende cache version
                 .filter(key => key !== staticCacheName)
+                // Map filter array og slet filer
                 .map(key => caches.delete(key)))
         })
     )
     return;
 })
 
+// Fetch event
 self.addEventListener('fetch', event => {
     // Fix af problem med dynamisk cache og chrome-extension bug
-    if(!(event.request.url.indexOf('http') === 0)) return;
+    if (!(event.request.url.indexOf('http') === 0)) return;
     // console.log(event.request);
 
     // Kontroller svar på request
